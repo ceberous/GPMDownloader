407be4a646f75785de1f2587f901270289616a7f
morpheous <cerbus.collin@gmail.com>
2016-08-10 10:37:38 -0700

    4

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
 	author = 'ceberous',
 	author_email = 'cerbus.collin@gmail.com',
 	url = 'https://github.com/ceberous/GPMDownloader.git',
+	download_url = 'https://github.com/ceberous/GPMDownloader/tarball/0.1',
 	keywords = [ 'google' , 'play' , 'music' , 'gmusicapi' , 'downloader' , 'playlist' ],
 	classifier = [],
 	install_requires = [ 'gmusicapi' , 'tqdm' ]
