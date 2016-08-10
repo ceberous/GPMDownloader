486efa5a7a2b214bb8d227a346614394302ff265
morpheous <cerbus.collin@gmail.com>
2016-08-10 11:19:18 -0700

    6

---
+++ GMDownloader/__init__.py
@@ -0,0 +1,2 @@
+import main2.py
+
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
 
 	name	= 'GMDownloader',
 	packages = [ 'GMDownloader' ],
-	version	= '0.2',
+	version	= '0.3',
 	description = 'GM Playlist Downloader',
 	long_description = open( 'README.txt' , 'rt' ).read(),
 	author = 'ceberous',
 	author_email = 'cerbus.collin@gmail.com',
 	url = 'https://github.com/ceberous/GPMDownloader.git',
-	download_url = 'https://github.com/ceberous/GPMDownloader/tarball/0.2',
+	download_url = 'https://github.com/ceberous/GPMDownloader/tarball/0.3',
 	keywords = [ 'google' , 'play' , 'music' , 'gmusicapi' , 'downloader' , 'playlist' ],
 
 	
